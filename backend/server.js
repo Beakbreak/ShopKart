0a7bd8b3f831f27b9f8cd1d810b2cf1cad489771
Vansh Mittal <mittalvansh69@gmail.com>
2023-08-12 15:02:59 +0530

    Initial Setup!

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const errorHandler = require("./middleware/error");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div>
+      <h1>React App</h1>
+    </div>
+  );
+}
